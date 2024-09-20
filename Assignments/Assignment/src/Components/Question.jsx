cd0fda2e899185ccbf7c499397c942117b89932e
Mayur Ashishbhai Ajugia <mayurajugia16@gmail.com>
2024-09-20 11:53:01 +0530

    task

---
+++ Assignments/Assignment/src/Components/Question.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+export default function Q_And_A() {
+  return (
+    <div>
+      <h1>Q&A</h1>
+      <h2>What is React Js? </h2>
+      <p>React is a open source library of javascript. it used to creat Ui of single page application</p><hr />
+
+      <h2>What is NPM in React Js? </h2>
+      <p>NPM is a Node Pakege Maneger</p><hr />
+
+      <h2>What is Role of Node Js in react Js? </h2>
+      <p>It’s majorly used to run applications outside the client browser. Node.js empowers developers to build robust, server-side applications that are fast and scalable.</p><hr />
+
+      <h2>What is CLI command In React Js? </h2>
+      <p>ReactJS - CLI Commands - React has its own command-line interface (CLI) commands. However, these CLI commands are currently only used to create a passable version of a react application using the command line</p><hr />
+
+      <h2>What is Components in React Js? </h2>
+      <p>Components are independent and reusable bits of code. </p><hr/>
+
+      <h2>What is Header and Content Components in React Js? </h2>
+      <p>The header is crucial to user interfaces, often housing navigation, branding, and other essential elements.</p><hr />
+
+      <h2> How to install React Js on Windows, Linux Operating System? How to Install 
+      NPM and How to check version of NPM? </h2><hr/>
+      <p>Node.js and npm installed: React relies on Node.js and npm (Node Package Manager) for managing dependencies and running build scripts.</p><hr/>
+
+      <h2>How to check version of React Js? </h2>
+      <p>There is another way to check the React version that is installed locally in your project. just type the below command and it will list all the packages with their version on the terminal window. 6.</p><hr />
+
+      <h2>How to change in components of React Js? </h2>
+      <p>Components in React return a piece of JSX code that tells what should be rendered on the screen</p><hr />
+
+    </div>
+  )
+}
